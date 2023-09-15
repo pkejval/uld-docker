15b6f4b9015cf907f18a5ed1a04b471dce9a1845
pkejval <petr.kejval6@gmail.com>
2023-09-15 14:37:31 +0200

    readable output

--- README.md
+++ README.md
@@ -5,10 +5,20 @@ This is container containing all required software to run uld-docker with PR htt
 # Installation
 # Building docker image yourself
 - Clone repo
-- Run command "docker build --tag uld-docker ."
-- Run command "docker run uld-docker args" where args are ulozto-downloader arguments. Hardcoded argument is "--auto-captcha" so there is not need to type it.
-In Windows do not forget to change line endings to LF ( see: https://stackoverflow.com/a/73028795) in run.sh otherwise you will encounter ``` exec /app/run.sh: no such file or directory```
+- Run command `docker build --tag uld-docker .`
+- Run command `docker run uld-docker "URL"` where URL is download link. Inputing ulozto-downloader arguments is not supported.
+
+In Windows do not forget to change line endings to LF (see: https://stackoverflow.com/a/73028795) in run.sh otherwise you will encounter ```exec /app/run.sh: no such file or directory```
 
 # From dockerhub
-- Run command "docker pull pkejval/uld-docker:main"
-- Run command "docker run pkejval/uld-docker args" where args are ulozto-downloader arguments. Hardcoded argument is "--auto-captcha" so there is not need to type it.
+- Run command `docker pull pkejval/uld-docker:main`
+- Run command `docker run pkejval/uld-docker "URL"` where URL is download link. Inputing ulozto-downloader arguments is not supported.
+
+
+# Configuration
+Because image isn't supporting inputting args directly to `ulozto-downloader` anymore, you can set it by envirnoment variables.
+
+### PARTS=number
+How many part split download to.
+### INTERVAL=number
+How often will script report status to STDOUT in seconds.
--- run.sh
+++ run.sh
@@ -1,28 +1,86 @@
 #!/bin/bash
 
-function exit_script(){
-  echo "Caught SIGTERM"
+OUT_INTERVAL=${OUT_INTERVAL:-10}
+PARTS=${PARTS:-20}
+
+trap exit_script SIGTERM
+trap exit_script SIGINT
+
+function exit_script() {
+  echo "Stopping script"
   exit 0
 }
 
-trap exit_script SIGTERM
+function run_uld() {
+    [[ $URL != http* ]] && continue
+    echo Starting ulozto-downloader for "$1"
+    echo ""
+
+    LAST_OUTPUT=0
+
+    (
+    python3 /app/ulozto-downloader/ulozto-downloader.py --frontend JSON --auto-captcha --parts $PARTS --output "/downloads" "$1"
+    ) | while read OUTPUT
+    do           
+        [[ $OUTPUT != {* ]] && continue
+        
+        if [[ $OUTPUT == {\"tor\":* ]]; then
+            MSG=$(jq -r '.tor' <<< $OUTPUT)
+
+            [[ $MSG =~ "Downloading GeoIP DB:".* ]] && continue
+
+            echo "Tor: $(jq -r '.tor' <<< $OUTPUT)"
+        
+        elif [[ $OUTPUT == {\"captcha\":*} ]]; then
+            echo "Captcha: $(jq -r '.captcha' <<< $OUTPUT)"
+
+        else
+            STATUS=$(jq -r '.status' <<< $OUTPUT)
+
+            if [[ $STATUS == "initializing" ]]; then
+                echo "ulozto-downloader is initializing..."
+                continue
+            fi
+
+            diff=$(($(date '+%s') - $LAST_OUTPUT))
+            [[ $diff -lt $OUT_INTERVAL ]] && continue
+
+            read -a arr < <(echo $(jq -r '.file, .url, .status, .curr_speed, .percent, .downloaded, .size, .remaining' <<< $OUTPUT))
+     
+            echo ----------
+            echo ""
+            echo "Timestamp:     $(date "+%D %T")"
+            echo ""
+            echo "File:          ${arr[0]}"
+            echo "URL:           ${arr[1]}"
+            echo ""
+            echo "Status:        ${arr[2]}"
+            echo "Current speed: ${arr[3]} ${arr[4]}"
+            echo "Downloaded:    ${arr[5]}${arr[6]} - ${arr[7]} ${arr[8]} of ${arr[9]} ${arr[10]}"
+            echo "Timeleft:      ${arr[11]}"
+            echo ""
+            echo ----------
+
+            LAST_OUTPUT=$(date '+%s')
+        fi
+    done
+}
 
 echo ""
 echo Starting FlareSolverr
+echo ""
 
 /app/flaresolverr/flaresolverr >/dev/null &
 
-echo Starting ulozto-downloader
-echo ""
-
 if [[ -f "/downloads/download.txt" ]]; then
+    echo ""
     echo "Downloading links from /downloads/download.txt"
     for URL in $(cat /downloads/download.txt)
     do
-        python3 /app/ulozto-downloader/ulozto-downloader.py --auto-captcha --output "/downloads" "$URL"
+        run_uld $URL
     done
 else
-    python3 /app/ulozto-downloader/ulozto-downloader.py --auto-captcha --output "/downloads" "$@"
+    run_uld $1
 fi
 
 exit 0
