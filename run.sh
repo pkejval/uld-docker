2a518be3e1732e35312a1f0868dfd6a11b0fcb4d
Liborsaf <37613703+Liborsaf@users.noreply.github.com>
2023-09-18 08:37:49 +0200

    Some fixes and enhancements (#5)
    * Correctly check url and return error message
    
    * Make shellcheck not complaining, add debug option & some fixes.
    
    * minor changes & fixed inconsistent tabs vs spaces
    
    ---------
    
    Co-authored-by: Petr Kejval <petr.kejval6@gmail.com>

--- run.sh
+++ run.sh
@@ -2,51 +2,56 @@
 
 OUT_INTERVAL=${INTERVAL:-10}
 PARTS=${PARTS:-20}
+DEBUG=${DEBUG:-0}
+
+[[ $DEBUG == 1 ]] && echo "Debug enabled!"
 
 trap exit_script SIGTERM
 trap exit_script SIGINT
 
 function exit_script() {
-  echo "Stopping script"
-  exit 0
+    echo "Stopping script"
+    exit 0
 }
 
 function run_uld() {
-    [[ $1 != http* ]] && continue
+    [[ $1 != http* ]] && echo "Skipping invalid URL $1" && return
     echo Starting ulozto-downloader for "$1"
     echo ""
 
     LAST_OUTPUT=0
 
     (
-    python3 /app/ulozto-downloader/ulozto-downloader.py --frontend JSON --auto-captcha --parts $PARTS --output "/downloads" "$1"
-    ) | while read OUTPUT
-    do           
-        [[ $OUTPUT != {* ]] && continue
+    python3 /app/ulozto-downloader/ulozto-downloader.py --frontend JSON --auto-captcha --parts "$PARTS" --output "/downloads" "$1"
+    ) | while read -r OUTPUT
+    do
+        [[ $DEBUG == 1 ]] && echo "$OUTPUT"
+
+        [[ $OUTPUT != \{* ]] && continue
         
-        if [[ $OUTPUT == {\"tor\":* ]]; then
-            MSG=$(jq -r '.tor' <<< $OUTPUT)
+        if [[ $OUTPUT == \{\"tor\":* ]]; then
+            MSG=$(jq -r '.tor' <<< "$OUTPUT")
 
             [[ $MSG =~ "Downloading GeoIP DB:".* ]] && continue
 
-            echo "Tor: $(jq -r '.tor' <<< $OUTPUT)"
+            echo "Tor: $(jq -r '.tor' <<< "$OUTPUT")"
         
-        elif [[ $OUTPUT == {\"captcha\":*} ]]; then
-            echo "Captcha: $(jq -r '.captcha' <<< $OUTPUT)"
+        elif [[ $OUTPUT == \{\"captcha\":*\} ]]; then
+            echo "Captcha: $(jq -r '.captcha' <<< "$OUTPUT")"
 
         else
-            STATUS=$(jq -r '.status' <<< $OUTPUT)
+            STATUS=$(jq -r '.status' <<< "$OUTPUT")
 
             if [[ $STATUS == "initializing" ]]; then
                 echo "ulozto-downloader is initializing..."
                 continue
             fi
 
-            diff=$(($(date '+%s') - $LAST_OUTPUT))
+            diff=$(($(date '+%s') - "$LAST_OUTPUT"))
             [[ $diff -lt $OUT_INTERVAL ]] && continue
 
-            read -a arr < <(echo $(jq -r '.file, .url, .status, .curr_speed, .percent, .downloaded, .size, .remaining' <<< $OUTPUT))
-     
+            mapfile -t arr < <(jq -r '.file, .url, .status, .curr_speed, .downloaded, .size, .percent, .remaining' <<< "$OUTPUT")
+
             echo ----------
             echo ""
             echo "Timestamp:     $(date "+%D %T")"
@@ -55,9 +60,9 @@ function run_uld() {
             echo "URL:           ${arr[1]}"
             echo ""
             echo "Status:        ${arr[2]}"
-            echo "Current speed: ${arr[3]} ${arr[4]}"
-            echo "Downloaded:    ${arr[5]}${arr[6]} - ${arr[7]} ${arr[8]} of ${arr[9]} ${arr[10]}"
-            echo "Timeleft:      ${arr[11]}"
+            echo "Current speed: ${arr[3]}"
+            echo "Downloaded:    ${arr[4]} of ${arr[5]} (${arr[6]})"
+            echo "Time left:     ${arr[7]}"
             echo ""
             echo ----------
 
@@ -75,12 +80,13 @@ echo ""
 if [[ -f "/downloads/download.txt" ]]; then
     echo ""
     echo "Downloading links from /downloads/download.txt"
-    for URL in $(cat /downloads/download.txt)
+
+    while IFS= read -r URL
     do
-        run_uld $URL
-    done
+        run_uld "$URL"
+    done < /downloads/download.txt
 else
-    run_uld $1
+    run_uld "$1"
 fi
 
 exit 0
